ae2dee13b68f202f54449c08040c57bc867e1faf
kyi0113 <KevinYi011@gmail.com>
2022-03-23 14:36:47 -0400

    update Q1 part B

--- Q1PartB.c
+++ Q1PartB.c
@@ -2,82 +2,93 @@
 #include <stdlib.h>
 #include <math.h>
 
-void calculate(int list[], int totalNumberOfProcess, FILE *file, int processorID, int pid[]);
+typedef struct
+{
+    int pid;
+    int burstime;
+    int memory;
+} process;
+
+void calculate(process list[], int totalNumberOfProcess, FILE *file, int processorID);
 
 void main()
 {
-
-    int k;
     float avg_WaitTime, avg_TurnaroundTime, avg_cycles, avg_MemoryFootprint, totalWaiting = 0, totalTurnAround = 0, totalCycle = 0, totalMemoryFootprint = 0;
     printf("This program is utilizing SJF algorithm:\n\n");
 
-    int totalNumberOfProcess = 250;
+    // File Variables
+    FILE *file;
+    FILE *fp;
 
-    // System has 250 processes
+    int totalNumberOfProcess = 50;
+
     int processorID[totalNumberOfProcess];
     int burstTimeList[totalNumberOfProcess];
-
     int waitingTime[totalNumberOfProcess];
     int turnAroundTime[totalNumberOfProcess];
 
-    int processor1[totalNumberOfProcess / 6];
-    int processor2[totalNumberOfProcess / 6];
-    int processor3[totalNumberOfProcess / 6];
-    int processor4[totalNumberOfProcess / 6];
-    int processor5[totalNumberOfProcess / 6];
-    int processor6[totalNumberOfProcess / 6];
+    process pArray[totalNumberOfProcess];
+    int count = 0;
+
+    process processor1[totalNumberOfProcess / 6];
+    process processor2[totalNumberOfProcess / 6];
+    process processor3[totalNumberOfProcess / 6];
+    process processor4[totalNumberOfProcess / 6];
+    process processor5[totalNumberOfProcess / 6];
+    process processor6[totalNumberOfProcess / 6];
 
     int readyList[totalNumberOfProcess];
 
     // Just initializing the processor array.
     for (int i = 0; i < totalNumberOfProcess / 6; i++)
     {
-        processor1[i] = -1;
-        processor2[i] = -1;
-        processor3[i] = -1;
-        processor4[i] = -1;
-        processor5[i] = -1;
-        processor6[i] = -1;
+        process temp = {-1, -1, -1};
+        processor1[i] = temp;
+        processor2[i] = temp;
+        processor3[i] = temp;
+        processor4[i] = temp;
+        processor5[i] = temp;
+        processor6[i] = temp;
     }
 
-    FILE *file;
+    // Open CSV with processes and store it into an array of structs
 
-    int burstTimeMax = (int)(pow(10, 6)); // x 10 ^ 6 cycles
-    int burstTimeMin = 10;                // x 10 ^ 6 cycles
-    int memoryMax = 16000;                // in MB
-    int memoryMin = 1;                    // in MB
-
-    // burstTimeLIst == readyLIst
-
-    // Generates 250 processes
-    for (int i = 0; i < totalNumberOfProcess; i++)
+    fp = fopen("processes.csv", "r");
+    if (fp == NULL)
     {
-        // the following generating random numbers in a range was found on:
-        // "https://www.geeksforgeeks.org/generating-random-number-range-c/"
-        int memory = (rand() % (memoryMax - memoryMin + 1)) + memoryMin;
-        int burstTime = (rand() % (burstTimeMax - burstTimeMin + 1)) + burstTimeMin;
-        processorID[i] = i;
-        readyList[i] = burstTime;
+        fprintf(stderr, "Error reading file\n");
     }
 
+    while (fscanf(fp, " %d,%d,%d", &pArray[count].pid, &pArray[count].burstime, &pArray[count].memory) == 3)
+    {
+        count++;
+    }
+
+    for (int i = 0; i < count; i++)
+    {
+        printf("%d,%d,%d\n", pArray[i].pid, pArray[i].burstime, pArray[i].memory);
+    }
+
+    fclose(fp);
+    printf("%d", pArray[3].burstime);
     // Orders the list, this greedy/brute force algorithm
     // is ordering it in Descending order from largest burst time to smallest.
     for (int j = 0; j < totalNumberOfProcess - 1; j++)
     {
         for (int l = j + 1; l < totalNumberOfProcess; l++)
         {
-            if (readyList[j] < readyList[l])
+            if (pArray[j].burstime < pArray[l].burstime)
             {
                 // Swaps the burstTime value in the correct order based on how fast
                 // the process terminates.
-                int temp = readyList[j];
-                readyList[j] = readyList[l];
-                readyList[l] = temp;
+                int temp = pArray[j].burstime;
+                pArray[j].burstime = pArray[l].burstime;
+                pArray[l].burstime = temp;
 
                 // Swaps and updates what ordering processor needs to be in.
-                int tempProcessor = processorID[j];
-                processorID[j] = processorID[l];
-                processorID[l] = tempProcessor;
+                int tempProcessor = pArray[j].pid;
+                pArray[j].pid = pArray[l].pid;
+                pArray[l].pid = tempProcessor;
             }
         }
     }
@@ -87,26 +98,17 @@ void main()
     // Re-Sorts the list into ascending order.
     while (pointer1 <= pointer2)
     {
-        int temp = readyList[pointer1];
-        readyList[pointer1] = readyList[pointer2];
-        readyList[pointer2] = temp;
+        int temp = pArray[pointer1].burstime;
+        pArray[pointer1].burstime = pArray[pointer2].burstime;
+        pArray[pointer2].burstime = temp;
 
         // Swaps and updates what ordering processor needs to be in.
-        int tempProcessor = processorID[pointer1];
-        processorID[pointer1] = processorID[pointer2];
-        processorID[pointer2] = tempProcessor;
+        int tempProcessor = pArray[pointer1].pid;
+        pArray[pointer1].pid = pArray[pointer2].pid;
+        pArray[pointer2].pid = tempProcessor;
         pointer1++;
         pointer2--;
     }
-
-    int pid1[totalNumberOfProcess / 6];
-    int pid2[totalNumberOfProcess / 6];
-    int pid3[totalNumberOfProcess / 6];
-    int pid4[totalNumberOfProcess / 6];
-    int pid5[totalNumberOfProcess / 6];
-    int pid6[totalNumberOfProcess / 6];
-
-    int i1 = 0, i2 = 0, i3 = 0, i4 = 0, i5 = 0, i6 = 0;
 
     int readyIndex = 0;
     int index = 0;
@@ -117,42 +119,29 @@ void main()
             index = 0;
         }
 
-        if (processor1[index] == -1)
+        if (processor1[index].pid == -1)
         {
-            processor1[index] = readyList[readyIndex];
-            pid1[i1] = processorID[readyIndex];
-            i1++;
+            processor1[index] = pArray[readyIndex];
         }
-        else if (processor2[index] == -1)
+        else if (processor2[index].pid == -1)
         {
-
-            processor2[index] = readyList[readyIndex];
-            pid2[i2] = processorID[readyIndex];
-            i2++;
+            processor2[index] = pArray[readyIndex];
         }
-        else if (processor3[index] == -1)
+        else if (processor3[index].pid == -1)
         {
-            processor3[index] = readyList[readyIndex];
-            pid3[i3] = processorID[readyIndex];
-            i3++;
+            processor3[index] = pArray[readyIndex];
         }
-        else if (processor4[index] == -1)
+        else if (processor4[index].pid == -1)
         {
-            processor4[index] = readyList[readyIndex];
-            pid4[i4] = processorID[readyIndex];
-            i4++;
+            processor4[index] = pArray[readyIndex];
         }
-        else if (processor5[index] == -1)
+        else if (processor5[index].pid == -1)
         {
-            processor5[index] = readyList[readyIndex];
-            pid5[i5] = processorID[readyIndex];
-            i5++;
+            processor5[index] = pArray[readyIndex];
         }
-        else
+        else if (processor6[index].pid == -1)
         {
-            processor6[index] = readyList[readyIndex];
-            pid6[i6] = processorID[readyIndex];
-            i6++;
+            processor6[index] = pArray[readyIndex];
         }
         index++;
         readyIndex++;
@@ -170,34 +159,34 @@ void main()
         if (j == 1)
         {
             // void calculate(processor1, 'processor1', totalNumberOfProcess, file);
-            calculate(processor1, totalNumberOfProcess, file, j, pid1);
+            calculate(processor1, totalNumberOfProcess, file, j);
         }
         else if (j == 2)
         {
-            calculate(processor2, totalNumberOfProcess, file, j, pid2);
+            calculate(processor2, totalNumberOfProcess, file, j);
         }
         else if (j == 3)
         {
-            calculate(processor3, totalNumberOfProcess, file, j, pid3);
+            calculate(processor3, totalNumberOfProcess, file, j);
         }
         else if (j == 4)
         {
-            calculate(processor4, totalNumberOfProcess, file, j, pid4);
+            calculate(processor4, totalNumberOfProcess, file, j);
         }
         else if (j == 5)
         {
-            calculate(processor5, totalNumberOfProcess, file, j, pid5);
+            calculate(processor5, totalNumberOfProcess, file, j);
         }
         else
         {
-            calculate(processor6, totalNumberOfProcess, file, j, pid6);
+            calculate(processor6, totalNumberOfProcess, file, j);
         }
     }
 
     fclose(file);
 }
 
-void calculate(int list[], int totalNumberOfProcess, FILE *file, int processorID, int pid[])
+void calculate(process list[], int totalNumberOfProcess, FILE *file, int processorID)
 {
     int length = totalNumberOfProcess / 6;
     int totalWaiting = 0;
@@ -216,7 +205,7 @@ void calculate(int list[], int totalNumberOfProcess, FILE *file, int processorID
 
             // we don't care about arrival times for this project.
             // W = Prev timeStamp
-            waitingTime[c] = waitingTime[c - 1] + list[r];
+            waitingTime[c] = waitingTime[c - 1] + list[r].burstime;
         }
 
         totalWaiting += waitingTime[c];
@@ -227,14 +216,10 @@ void calculate(int list[], int totalNumberOfProcess, FILE *file, int processorID
     // Calculates the TurnAroundTime
     for (int f = 0; f < length; f++)
     {
-
         // TR = W + Burst Time
-
-        turnAroundTime[f] = list[f] + waitingTime[f];
-
+        turnAroundTime[f] = list[f].burstime + waitingTime[f];
         totalTurnAround += turnAroundTime[f];
-
-        fprintf(file, "\n p:%d, TurnAround Time: %d, Waiting Time: %d, ", pid[f], turnAroundTime[f], waitingTime[f]);
+        fprintf(file, "\n p:%d, TurnAround Time: %d, Waiting Time: %d, ", list[f].pid, turnAroundTime[f], waitingTime[f]);
     }
 
     avg_TurnaroundTime = ((float)totalTurnAround) / length;
